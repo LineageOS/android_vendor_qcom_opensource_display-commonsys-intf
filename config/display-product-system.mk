9356417fe1cfac9ffbb9ea0b3f2535ebbb20bd68
Devanshi Bansal <quic_devanshi@quicinc.com>
2023-09-05 12:33:01 +0530

    commonsys-intf-display: Update display config to V10
    Change-Id: I51370578681887dfcfcd8ead5b1aefb30c783710

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -9,4 +9,8 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V3-ndk \
                     vendor.qti.hardware.display.config-V4-ndk \
                     vendor.qti.hardware.display.config-V5-ndk \
-                    vendor.qti.hardware.display.config-V6-ndk
+                    vendor.qti.hardware.display.config-V6-ndk \
+                    vendor.qti.hardware.display.config-V7-ndk \
+                    vendor.qti.hardware.display.config-V8-ndk \
+                    vendor.qti.hardware.display.config-V9-ndk \
+                    vendor.qti.hardware.display.config-V10-ndk
