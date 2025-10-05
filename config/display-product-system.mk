bacf3619263d136d35d3a137557431f91fcd1455
Avdhesh Singh Chauhan <avdheshs@qti.qualcomm.com>
2025-10-06 01:49:57 +0530

    commonsys-intf: IDisplayConfig: Update config AIDL interface
    - Add set pose config API support.
    - Add DisplayConfig V15 to PRODUCT_PACKAGES.
    
    Change-Id: I4ebaa8b859360e099e49d4f70ecc10d6504a5184

--- config/display-product-system.mk
+++ config/display-product-system.mk
@@ -17,4 +17,5 @@ PRODUCT_PACKAGES += vendor.display.config@1.0 \
                     vendor.qti.hardware.display.config-V11-ndk \
                     vendor.qti.hardware.display.config-V13-ndk \
                     vendor.qti.hardware.display.config-V14-ndk \
+                    vendor.qti.hardware.display.config-V15-ndk \
                     vendor.qti.hardware.display.snapallocext-V1-ndk
