8522ad2c832c6b9f7f61c6c2fade3faec7ff1e18
Tommy Sandanasamy <tsandana@qti.qualcomm.com>
2025-11-05 10:26:08 -0800

    gralloc: Add new ROI_METADATA type to list of supported metadata
    Change-Id: I7e8a1afd8ba8af00e9bf85f25cb25f77fe0690c4

--- gralloc/mapperInterfaces/MetadataType.h
+++ gralloc/mapperInterfaces/MetadataType.h
@@ -388,7 +388,12 @@ typedef enum vendor_qti_hardware_display_common_MetadataType {
    * Concurrent Write Back Metadata
    * Functions supported: getMetadata, setMetadata
    */
-  CWB_METADATA = 10052
+  CWB_METADATA = 10052,
+  /**
+   * ROI metadata,
+   * Functions supported: getMetadata, setMetadata
+   */
+  ROI_RECT_METADATA = 10053
 
 } vendor_qti_hardware_display_common_MetadataType;
 
